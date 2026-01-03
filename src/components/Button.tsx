dab9791fe7a46c1beb9f7c968425d878c2367d48
simmetr <vm.certo@gmail.com>
2026-01-03 13:23:12 +0700

    style: добавлен active

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import './button.scss';
 import './button--hover.scss';
+import './button--active.scss';
 import './properties-when-the-icon-is-present/button--with-icon.scss';
 
 type ButtonProps = {
@@ -11,7 +12,7 @@ export const Button = ({value, nameOfIcon}: ButtonProps) => {
     const pathToTheFolderWithIconsForButtons: string = '/images/buttons/';
     return (
         <button type='button'
-                className={`button button--hover ${nameOfIcon ? 'button--with-icon' : ''}`}
+                className={`button button--hover button--active ${nameOfIcon ? 'button--with-icon' : ''}`}
                 style={nameOfIcon ? {['--icon-url' as any]: `url(${pathToTheFolderWithIconsForButtons}${nameOfIcon})`} : undefined}>{value}</button>
     );
 }
