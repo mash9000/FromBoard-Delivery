c82c5bc5118dc4d8d8283d9cbe7934699c08a47d
simmetr <vm.certo@gmail.com>
2026-01-07 13:15:23 +0700

    refactor: link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,6 +1,6 @@
-import './styles/link.scss';
-import './styles/link--hover.scss';
-import './styles/link--active.scss';
+import './link/link.scss';
+import './link/_hover/link--hover.scss';
+import './link/_active/link--active.scss';
 import type {LinkModel} from "./model/LinkModel.ts";
 
 export const Link = ({getHref, getTitle}: LinkModel) => {
