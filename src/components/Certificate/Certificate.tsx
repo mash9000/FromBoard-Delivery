3e655d65f5aff60d6d1e780b7f2a848a8fe2d3aa
simmetr <vm.certo@gmail.com>
2026-01-11 00:21:25 +0700

    style: "гарантии качества" стилизованы по дефолту

--- src/components/Certificate/Certificate.tsx
+++ src/components/Certificate/Certificate.tsx
@@ -1,14 +1,20 @@
+import './certificate/certificate.scss';
+import './certificate/__img/certificate__img.scss';
+import './certificate/__title/certificate__title.scss';
+
 import type {
     QualityDocument
 } from "./model/QualityDocument.ts";
 
 export const Certificate = (certificate: QualityDocument) => {
     return (
-        <figure key={certificate.title}>
+        <figure className="certificate">
             <img
                 src={certificate.imgSrc}
-                alt={certificate.imgAlt}/>
-            <figcaption>{certificate.title}</figcaption>
+                alt={certificate.imgAlt}
+                className='certificate__img'/>
+            <figcaption
+                className='certificate__title'>{certificate.title}</figcaption>
         </figure>
     );
 }
--- src/components/QualityGuarantees/QualityGuarantees.tsx
+++ src/components/QualityGuarantees/QualityGuarantees.tsx
@@ -1,5 +1,8 @@
 import './quality-guarantees/quality-guarantees.scss';
 import './quality-guarantees/__inner-box/quality-guarantees__inner-box.scss';
+import './quality-guarantees/__heading/quality-guarantees__heading.scss';
+import './quality-guarantees/__desc/quality-guarantees__desc.scss';
+
 import {Button} from "../Button/Button.tsx";
 import type {QualityGuaranteesProps} from "./model/QualityGuaranteesProps.ts";
 import type {QualityDocument} from "../Certificate/model/QualityDocument.ts";
@@ -8,9 +11,9 @@ import {Certificate} from "../Certificate/Certificate.tsx";
 export const QualityGuarantees = (data: QualityGuaranteesProps) => {
     return (
         <div className="quality-guarantees">
+            <h3 className='quality-guarantees__heading'>{data.heading}</h3>
+            <p className='quality-guarantees__desc'>{data.description}</p>
             <div className="quality-guarantees__inner-box">
-                <h3>{data.heading}</h3>
-                <p>{data.description}</p>
                 {data.certificates.map((certificate: QualityDocument) => (
                     <Certificate key={certificate.title} {...certificate} />))}
             </div>
